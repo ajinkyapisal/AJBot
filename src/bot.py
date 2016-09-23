5ad09750ecd6700ea1f96e8cc1a3c2c117935e08
Ajinkya <ajinkya.pisal@healthfidelity.com>
2016-09-24 03:13:18 +0530

    initial commit for AJBot

---
+++ src/bot.py
@@ -0,0 +1,36 @@
+import sys,os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import config
+import socket
+import re
+import time
+
+CHAT_MSG=re.compile(r"^:\w+!\w+@\w+\.tmi\.twitch\.tv PRIVMSG #\w+ :")
+
+s = socket.socket()
+s.connect((config.HOST, config.PORT))
+s.send("PASS {}\r\n".format(config.PASS).encode("utf-8"))
+s.send("NICK {}\r\n".format(config.NICK).encode("utf-8"))
+s.send("JOIN {}\r\n".format(config.CHANNEL).encode("utf-8"))
+
+def chat(sock, msg):
+    sock.send("PRIVMSG #{} :{}".format(config.CHANNEL, msg))
+
+def ban(sock, user):
+    chat(sock, ".ban {}".format(user))
+
+def timeout(sock, user, sec=600):
+    chat(sock, ".timeout {}".format(user, sec))
+
+while True:
+    response = s.recv(1024).decode("utf-8")
+    if response == "PING :tmi.twitch.tv\r\n":
+        s.send("PONG :tmi.twitch.tv\r\n".encode("utf-8"))
+    else:
+        username = re.search(r"\w+", response).group(0)
+        message = CHAT_MSG.sub("", response)
+        if username == config.NICK:
+            print("me: " + message)
+        else:
+            print(username + ": " + message)
+    time.sleep(0.1)
